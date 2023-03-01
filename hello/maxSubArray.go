6724a44129ab49bdf07862fab5764ed625d8e9f7
kingykwang <kingykwang@tencent.com>
2023-03-01 11:13:18 +0800

    最大子数组和

---
+++ hello/maxSubArray.go
@@ -0,0 +1,31 @@
+package main
+
+// 输入：nums = [-2,1,-3,4,-1,2,1,-5,4]
+// 输出：6
+// 解释：连续子数组 [4,-1,2,1] 的和最大，为 6 。
+
+func maxSubArray(nums []int) int {
+	res := nums[0]
+	n := len(nums)
+	dp := make([]int, n)
+	dp[0] = nums[0]
+	for k, v := range nums {
+		if k == 0 {
+			continue
+		}
+		if dp[k-1] >= 0 {
+			dp[k] = dp[k-1] + v
+		} else {
+			dp[k] = v
+		}
+		res = max(res, dp[k])
+	}
+
+	return res
+}
+func max(x, y int) int {
+	if x > y {
+		return x
+	}
+	return y
+}
